e98d6ed17184b090375cb91159a10898f4464127
Wakar Seraj Khan <wakar473@gmail.com>
2024-12-03 13:02:20 +0530

    feat:added controller module

---
+++ controllers/controllers.go
@@ -0,0 +1,30 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword (password string) string {
+		
+}
+
+
+func VerifyPassword (userPassword string, givenPassword string) (bool, string){
+
+}
+
+func signup () gin.HandlerFunc{
+
+}
+
+func login () gin.HandlerFunc{
+	
+}
+
+func ProductViewerAdmin() gin.HandlerFunc{
+
+}
+
+func SearchProduct() gin.HandlerFunc{
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc
