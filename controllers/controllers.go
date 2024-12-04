0fc7079088705f6c8956dc4f7276285a3dba3717
Wakar Seraj Khan <wakar473@gmail.com>
2024-12-04 11:10:23 +0530

    feat:added signup feature

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wakar473/Ecomerce-Website/models"
+)
 
 func HashPassword (password string) string {
 		
@@ -12,6 +20,39 @@ func VerifyPassword (userPassword string, givenPassword string) (bool, string){
 }
 
 func signup () gin.HandlerFunc{
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel ()
+		var user models.User
+		if err := c.BindJSON(&user); err!= nil{
+			c.JSON{http.StatusBadRequest, gin.H{"error": err.Error()}}
+			return
+		}
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email":user.Email})
+		if err != nil{
+			log.Panic(err)
+			c.json(http.StatusBadRequest, gin.H{"error":"user already exists"})
+		}
+		Count,err = UserCollection.CountDocuments(ctx, bson.M{"phone":user.Phone})
+
+		defer cancel()
+		if err!= nil{
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError,gin.H{"error": err})
+			return
+		}
+
+		if count>0{
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone no is already in use"})
+			return
+		}
+
+	}
 
 }
 
---
+++ database/cart.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	ErrCantFindProduct = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("cant find the product")
+    ErrUserIdIsNotValid = errors.New("this user is not valid")
+    ErrCantUpdateUser = errors.New("cannot add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("cannot remove this item from the cart")
+	ErrCanGetItem = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCarItem = errors.New("cannot update the purchase")
+)
+
+func AddToCart()gin.HandlerFunc {
+
+}
+
+func RemoteItem() gin.HandlerFunc{
+
+}
+
+func GetItemFromCart() gin.HandlerFunc{
+
+}
+
+func BuyFromCart() gin.HandlerFunc{
+
+}
+func InstantBuy() gin.HandlerFunc{
+	
+}
+
