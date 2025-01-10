344a5a38590d661059f63f54f0ba4e629986bc02
Wakar Seraj Khan <wakar473@gmail.com>
2025-01-10 11:53:53 +0530

    feat:controllers/controllers.go/added functionality of SearchProductQuery

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -145,7 +145,83 @@ func ProductViewerAdmin() gin.HandlerFunc{
 }
 
 func SearchProduct() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+		var productlist []models.Product
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)	
+		defer cancel()
+
+		ProductCollection.Find(ctx, bson.D{{}})
+		if err! = nil {
+			c.IndentedJSON(http.StatusInternalServerError, "something went wrong, please try after some time")
+			return
+
+		}
+
+		err = cursor.All(ctx, &productlist)
+
+		if err!= nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		defer cursor.Close()
+		if err := cursor.err(); err != nil {
+			log.Println(err)
+			c.IndentedJSON(400, "invalid")
+			return
+		}
+		defer cancel()
+		c.IndentedJSON(200, productlist)
+	}
 
 }
 
-func SearchProductByQuery() gin.HandlerFunc
+func SearchProductByQuery() gin.HandlerFunc{
+	return func( c *gin.Context){
+		var searchProducts []models.Product
+		queryParam := c.Query("name")
+
+		//you want to check if it's empty
+
+		if queryParam == ""{
+			log.Println("query is empty")
+			c.Header("Content-Type", "application/json")
+			c.JSON(HTTP.StatusNotFound, gin.H{"Error":"Invalid search index"})
+			c.Abort()
+			return
+
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		searchquerydb, err := ProductCollection.Find(ctx, bson.M{"product_name": bson.M{"$regex":queryParam}})
+
+		if err != nil {
+			c.IndentedJSON(404, "something went wrong while fetching the data")
+			return
+		}
+
+		err = searchquerydb.All(ctx, &searchProducts)
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(400, "invalid")
+			return
+		}
+
+		defer searchquerydb.Close(ctx)
+
+		if err := searchquerydb.Err(); err != nil {
+			log.Println(err)
+			c.IndentedJSON(400, "invalid request")
+			return
+		}
+
+		defer cancel()
+		c.IndentedJSON(200,searchProducts)
+	}
+
+
+}
