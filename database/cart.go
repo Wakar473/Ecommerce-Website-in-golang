5d7d001de768250f256584e74ed044e62c51e979
wakar473 <wakar473@gmail.com>
2025-03-07 10:47:21 +0530

    feat:added AddProductToCart function-database/cart.go

--- database/cart.go
+++ database/cart.go
@@ -1,9 +1,14 @@
 package database
 
 import (
+	"context"
 	"errors"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wakar473/Ecommerce-Website/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
@@ -16,7 +21,33 @@ var (
 	ErrCantBuyCarItem = errors.New("cannot update the purchase")
 )
 
-func AddProductToCart()gin.HandlerFunc {
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID,userID string) error { 
+	searchfromdb, err := prodCollection.Find(ctx, bson.M{"_id":productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchfromdb.All(ctx, &productcart)
+	if err != nil {
+		log.Print(err)
+		return ErrCantDecodeProducts
+	}
+
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{primitive.E{key:"_id", Value: id}}
+	update := bson.D{{Key:"$push", Value: bson.D{primitive.E{key:"usercart", Value: bson.D{{key:"$each", Value:productCart}}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
 
 }
 
