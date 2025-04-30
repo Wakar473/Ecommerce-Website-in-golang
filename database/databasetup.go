83e02dd41bc383a837554e30effe47dd0064a1ce
wakar644 <wakar.seraj@antiersolutions.com>
2025-04-30 13:14:20 +0530

    fix:database errors

--- database/databasetup.go
+++ database/databasetup.go
@@ -10,24 +10,41 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// func DBSet() *mongo.Client {
+// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	err = client.Ping(context.TODO(), nil)
+// 	if err != nil {
+// 		log.Println("failed to connect to mongodb")
+// 		return nil
+// 	}
+// 	fmt.Println("Successfully Connected to the mongodb")
+// 	return client
+// }
+
 func DBSet() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	err = client.Connect(ctx)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("MongoDB connection error: %v", err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Println("failed to connect to mongodb")
-		return nil
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("MongoDB ping error: %v", err)
 	}
-	fmt.Println("Successfully Connected to the mongodb")
+
+	fmt.Println("Successfully connected to MongoDB")
 	return client
 }
 
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/validator/v10 v10.20.0
-	go.mongodb.org/mongo-driver v1.17.1
+	go.mongodb.org/mongo-driver v1.17.3
 	golang.org/x/crypto v0.26.0
 )
 
@@ -22,7 +22,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
