ab87df7a7f75c40782a9a866745d5214aafca99d
wakar473 <wakar473@gmail.com>
2025-04-30 13:28:28 +0530

    chore:clean the code

--- database/databasetup.go
+++ database/databasetup.go
@@ -10,27 +10,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// func DBSet() *mongo.Client {
-// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 	defer cancel()
-// 	err = client.Connect(ctx)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	err = client.Ping(context.TODO(), nil)
-// 	if err != nil {
-// 		log.Println("failed to connect to mongodb")
-// 		return nil
-// 	}
-// 	fmt.Println("Successfully Connected to the mongodb")
-// 	return client
-// }
-
 func DBSet() *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
